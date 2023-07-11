ead9307049dd7d00b56b80b8f801f82709903ce9
ubolts <ubolts7777@gmail.com>
2023-07-11 17:38:59 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,37 @@
-print("Hello")
+'''
+print("hello")
+print(1)
+print(1.2)
+print(7j+ 8j)
+print(True)
+print(False)
+
+print(type("hello"))
+print(type(1))
+print(type(1.22))
+print(type(True))
+
+print(1+1)
+print(1.2+1)
+print("hello" + "yuu")
+
+
+operator
++,-,*,/
+
+
+print(2!=2)
+
+x= 6
+y = "hello"
+
+print(x + y)
+x = int(input())
+print(type(x))
+'''
+'''calculator'''
+ 
+a = 10
+b = 10
+c = a / b
+print(type(c))
