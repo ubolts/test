afa2377b9e3b4e17033e2580ff7e352bf09de55a
ubolts <ubolts7777@gmail.com>
2023-07-13 17:08:34 +0700

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,67 @@
+# # # if True:
+# # #     print("hello")
+
+# # # if False:
+# # #     print("hello1")
+
+# # #     money = 100
+
+# # #     if money >= 300:
+# # #         print("KFC!!!")
+# # #     else:
+# # #         print("byebye")
+        
+# # login = input("name")
+
+# # password = input("Password")
+# # if password == ("1234") and login ==("Yuu"):
+# #     print ("True")
+# # else:
+# #     print("False")
+
+    
+# # # x = 10
+# # # y = 10
+# # # # print (x == y)
+# # # (x != y)
+# # # # print(x != y)
+# # # (x < y)
+# # # print (x <= y)
+# # # print (x >= y)
+
+# x = int(input())
+# y = int(input())
+
+# if x*10 == y:
+#     print("True")
+# else:
+#     print("False")
+
+# user = input("Enter your username")
+# passw = input("Enter your password")
+
+# user = input("name")
+# passw = input("password")
+# if passw == ("1234") and user == ("Zontee"):
+#     print ("Zontee is stupid")
+# elif user == ("Mew"):
+#     print ("Mew is stupid")
+# else:
+#     print("False")]
+# i = 0
+# x = 1
+# while (x<53):
+#     i += 1
+#     print("hello")
+#     x *= 2
+# print("2")
+# print(i)
+
+while True:
+    name = input("name") 
+    password = input("password")
+    if name == ("Yuu") and password == ("1234"):
+        print("True")
+        break
+    else:
+        print("False")
