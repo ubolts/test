f4906dc3cc57aeafa511a7f68fc444785037b7d5
ubolts <ubolts7777@gmail.com>
2023-07-20 17:13:36 +0700

    Create cal2.py

---
+++ cal2.py
@@ -0,0 +1,19 @@
+def add(d1:int, d2:int):
+    return d1+d2
+
+def divide(d1:int, d2:int):
+    return d1/d2
+
+def subtract(d1:int, d2:int):
+    return d1-d2
+
+def multiply(d1:int, d2:int):
+    return d1*d2
+
+num1 = int(input('num1:'))
+num2 = int(input('num2:'))
+    
+print(add(num1, num2))
+print(divide(num1, num2))
+print(subtract(num1, num2))
+print(multiply(num1, num2))
